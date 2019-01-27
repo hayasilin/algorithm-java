e2081f92b424d9bd11ed166a2f880e028459492d
hayasilin <harvey.hayasi@gmail.com>
2019-01-27 12:35:07 +0800

    add plus one

--- src/leetcode/RemoveElement.java
+++ src/leetcode/RemoveElement.java
@@ -1,7 +1,7 @@
 package leetcode;
 
 public class RemoveElement {
-	static int[] data = {1, 2, 2, 3, 2, 4};
+	static int[] data = {1, 1, 2, 3};
 	
 	static int removeElement(int[] data, int n, int element) {
 		int i = 0;
@@ -18,8 +18,25 @@ public class RemoveElement {
 		return j;
 	}
 	
+	static int removeDuplicatesFromSortedArray(int[] nums, int n) {
+		if (n == 0) {
+			return 0;
+		}
+		
+		int j = 0;
+		for (int i = 1; i < n; i++) {
+			if (nums[j] != nums[i]) {
+				nums[++j] = nums[i];
+			}
+		}
+		return j+1;
+	}
+	
 	public static void main(String[] args) {
-		int newLength = removeElement(data, data.length, 2);
-		System.out.println(newLength);
+		//int newLength = removeElement(data, data.length, 2);
+		//System.out.println(newLength);
+		
+		int result = removeDuplicatesFromSortedArray(data, data.length);
+		System.out.println(result);
 	}
 }
