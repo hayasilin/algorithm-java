931244fb6389da825f622b9b8a79cae66b247bb5
kuanwei.lin <kuanwei.lin@linecorp.com>
2019-05-19 13:16:15 +0800

    update perfect number

--- src/leetcode/PerfectNumber.java
+++ src/leetcode/PerfectNumber.java
@@ -33,8 +33,27 @@ public class PerfectNumber {
         return sum == num;
     }
 	
+	public static boolean checkPerfectNumberFaster(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if (i * i != num) {
+                    sum += num / i;
+                }
+
+            }
+        }
+        return sum - num == num;
+    }
+	
 	public static void main(String[] args) {
 		System.out.println(checkPerfectNumber(28));
 		printAllPerfectNumber(500);
+		System.out.println(checkPerfectNumberFaster(28));
+		
 	}
 }
